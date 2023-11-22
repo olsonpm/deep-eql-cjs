a33650da4cb582ce06d30e1d493a46ac6314dc9e
Phil Olson <olsonpm@users.noreply.github.com>
2023-11-22 17:32:17 -0600

    account for default

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-module.exports = require('fix-esm').require('deep-eql')
+const mod = require('fix-esm').require('deep-eql')
+
+const defExport = mod.default
+
+for (const [k, v] of Object.entries(mod)) {
+  if (k === 'default') continue
+  defExport[k] = v
+}
+
+module.exports = defExport
